82a2ce01a9a7df793ea73455accee7d34db4545f
andersonVJ <andersonvalencia23j@gmail.com>
2025-03-28 11:33:45 -0500

    Models

--- Gestion_Recursos/recursos/admin.py
+++ Gestion_Recursos/recursos/admin.py
@@ -13,3 +13,4 @@ class SolicitudRecursoAdmin(admin.ModelAdmin):
     list_display = ("usuario", "recurso", "cantidad_solicitada", "fecha_solicitud", "estado")
     list_filter = ("estado", "fecha_solicitud")
     search_fields = ("usuario__username", "recurso__nombre")
+    
